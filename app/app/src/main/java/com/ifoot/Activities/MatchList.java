5e1c1907b87ac9e3620bdfb5647ec825efa9da12
Adrian Grosch <adrian.grosch@singular.inf.br>
2021-07-25 01:24:02 -0300

    criação de partida sem pickers

--- app/app/src/main/java/com/ifoot/Activities/MatchList.java
+++ app/app/src/main/java/com/ifoot/Activities/MatchList.java
@@ -2,22 +2,60 @@ package com.ifoot.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 
 
 import com.ifoot.Models.Match;
 import com.ifoot.R;
+import com.ifoot.Services.Match.Create;
 import com.ifoot.Services.Match.FecthAll;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MatchList extends AppCompatActivity {
+    EditText date;
+    EditText time;
+
+    public void transformInputs() {
+        this.date = findViewById(R.id.newMatchDateInput);
+        this.time = findViewById(R.id.newMatchTimeInput);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateDialog(view, date);
+            }
+        });
+    }
+
+    private void showDateDialog(View view, EditText date) {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR, i);
+                calendar.set(Calendar.MONTH, i1);
+                calendar.set(Calendar.DAY_OF_MONTH, i2);
+                SimpleDateFormat matchDate = new SimpleDateFormat("yyyy-MM-dd");
+                date.setText(matchDate.format(calendar));
+            }
+        };
+        new DatePickerDialog(view.getContext(), dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +64,7 @@ public class MatchList extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         ArrayAdapter<Match> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, matches);
         listView.setAdapter(adapter);
-
+        this.transformInputs();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -35,6 +73,17 @@ public class MatchList extends AppCompatActivity {
                 intent.putExtra("id", item.getId());
                 startActivityForResult(intent, 0);
             }
+        });
+
+        Button newMatchButton = (Button) findViewById(R.id.newMatchButton);
+        newMatchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText location = findViewById(R.id.newMatchLocationInput);
+                new Create().startTask(location.getText().toString(), date.getText() + " " + time.getText());
+                finish();
+                startActivity(getIntent());
+            }
+
         });
     }
 }
---
+++ app/app/src/main/java/com/ifoot/Services/Match/Create.java
@@ -0,0 +1,53 @@
+package com.ifoot.Services.Match;
+
+import android.os.AsyncTask;
+
+import com.ifoot.Models.User;
+import com.ifoot.Services.User.Subscribe;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Create {
+    public boolean startTask(String location, String date) {
+        try {
+            return new CreateTask().execute(location, date).get();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    class CreateTask extends AsyncTask<String, String, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                User user = User.getInstance();
+
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .retryOnConnectionFailure(true)
+                        .build();
+
+                RequestBody formBody = new FormBody.Builder()
+                        .add("location", params[0])
+                        .add("date", params[1])
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://10.0.2.2/api/match/")
+                        .addHeader("Authorization", "token " + user.getToken())
+                        .post(formBody)
+                        .build();
+                Response response = client.newCall(request).execute();
+                return response.code() == 200;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+}
